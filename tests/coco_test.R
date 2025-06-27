9f33e76a8b42a67caa464638911fe6cb577ab1ac
Federico Blasi <33967974+blasif@users.noreply.github.com>
2025-06-27 09:22:31 +0000

    more tests

--- tests/coco_test.R
+++ tests/coco_test.R
@@ -22,6 +22,8 @@ obj <- coco(type = "dense",
             z = data_test$z,
             model.list = model.list)
 
+obj@info$lambda <- c(0,0,0)
+
 # Start manual checking
 stopifnot(inherits(obj, "coco"))
 
@@ -35,14 +37,30 @@ predss <- cocoPredict(test_optim, newdataset = cocons::holes$test[1:50,1:4],newl
 stopifnot(all(predss$systematic == 0))
 stopifnot(all(!is.na(predss$stochastic)))
 
+model.list <- list(
+  'mean'    = formula(~ 1 + cov_x + cov_y),
+  'std.dev' = ~ 1,
+  'scale'   = ~ 1,
+  'aniso'   = 0,
+  'tilt'    = 0,
+  'smooth'  = 1.5,
+  'nugget'  = -Inf
+)
+
+obj <- coco(type = "dense", 
+            data = data_test[,1:4], 
+            locs = data_test[,1:2], 
+            z = data_test$z,
+            model.list = model.list)
+
 test_optim <- cocoOptim(obj, ncores = 1, optim.type = 'pml')
 
 cmat <- getCovMatrix(test_optim)
 stopifnot(is.matrix(cmat), nrow(cmat) == 50, ncol(cmat) == 50)
 stopifnot(all(eigen(cmat)$values > 0))
 
-predss <- cocoPredict(test_optim, newdataset = cocons::holes$test[1:50,1:4],newlocs = as.matrix(holes$test[1:50,1:2]))
-stopifnot(all(predss$systematic == 0))
+predss <- cocoPredict(test_optim, newdataset = cocons::holes$test[1:50,1:4], newlocs = as.matrix(holes$test[1:50,1:2]))
+stopifnot(all(!is.na(predss$systematic)))
 stopifnot(all(!is.na(predss$stochastic)))
 
 # Spam and sparse coco type
